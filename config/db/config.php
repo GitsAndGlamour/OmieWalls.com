b4ae64e7fe85bf9dc75738f7cfa58fdd628513c9
GitsAndGlamour <omierwalls@gmail.com>
2016-03-15 09:45:45 -0400

    Database & Routing Initialized

---
+++ config/db/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Database Configurations */
+ define( 'DB_HOST', 'localhost' ); 
+ define( 'DB_USER', 'root' ); 
+ define( 'DB_PASS', 'siC10Ne.dr4mp' );
+ define( 'DB_NAME', 'domain' );
+ define( 'SEND_ERRORS_TO', 'omiewalls@outlook.com' ); 
+ define( 'DISPLAY_DEBUG', true ); 
+
+?>
